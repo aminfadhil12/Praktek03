a9ff8051fa64dff7af732eaf6664df3cf22f91bd
aminfadhil12 <aminfadhil.fadhil268@gmail.com>
2018-04-11 16:04:45 +0800

    Buat Getter And Setter

--- src/praktek03/Sepatu.java
+++ src/praktek03/Sepatu.java
@@ -10,4 +10,31 @@ public class Sepatu {
                 "Warna \t: "+warna+"\n"+
                 "Harga \t: "+harga+"\n");
     }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public void setMerk(String merk) {
+        this.merk = merk;
+    }
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+    
+    
+    
 }
