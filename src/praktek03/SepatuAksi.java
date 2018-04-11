f93f8044017a26d887eef21ac7359b1f1407b45d
aminfadhil12 <aminfadhil.fadhil268@gmail.com>
2018-04-11 16:08:15 +0800

    menggunakan method set

--- src/praktek03/SepatuAksi.java
+++ src/praktek03/SepatuAksi.java
@@ -4,9 +4,9 @@ package praktek03;
 public class SepatuAksi {
     public static void main(String[] args) {
         Sepatu s = new Sepatu();
-        s.merk="adidas";
-        s.warna="hitam";
-        s.harga=100000;
+        s.setMerk("Nike");
+        s.setWarna("Merah");
+        s.setHarga(50000);
         
         s.cetakInfo();
     }
